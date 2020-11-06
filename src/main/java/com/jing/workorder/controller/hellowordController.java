45ad99936493ad789a0543efa493e8ce5e94bbf1
jingtian.wang <jingtian.wang@united-imaging.com>
2020-11-06 18:46:57 +0800

    update

--- src/main/java/com/jing/workorder/controller/hellowordController.java
+++ src/main/java/com/jing/workorder/controller/hellowordController.java
@@ -2,10 +2,12 @@ package com.jing.workorder.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/api")
 @Api(tags = "欢迎页面")
@@ -14,6 +16,7 @@ public class hellowordController {
     @RequestMapping(value = "helloword", method = {RequestMethod.GET})
     @ApiOperation("接口测试")
     public String helloword () {
+        log.info("hello word");
         return "this is workorder hello word!";
     }
 }
---
+++ src/main/java/com/jing/workorder/util/UUIDUtils.java
@@ -0,0 +1,40 @@
+package com.jing.workorder.util;
+
+import java.util.UUID;
+
+/**
+ * @description: UUIDUtils
+ * @author: jingtian.wang
+ * @time: 2020/11/6 15:59
+ */
+public class UUIDUtils {
+    /**
+     * 获取UUID
+     *
+     * @return
+     */
+    public static String getUUID() {
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString().replaceAll("-", "");
+    }
+
+    /**
+     * 获取UUID
+     *
+     * @return
+     */
+    public static String[] getUUID(int number) {
+        String[] result = null;
+        if (number > 0) {
+            result = new String[number];
+            for (int i = 0; i < number; i++) {
+                result[i] = getUUID();
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getUUID());
+    }
+}
