ad9153c780c2e69f434510900d06c999e1623d66
jingtian.wang <jingtian.wang@united-imaging.com>
2020-11-19 20:20:01 +0800

    add user entity and table

---
+++ src/main/java/com/jing/workorder/mapper/UserAccountMapper.java
@@ -0,0 +1,16 @@
+package com.jing.workorder.mapper;
+
+import com.jing.workorder.entity.UserAccount;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 系统用户账号表 Mapper 接口
+ * </p>
+ *
+ * @author jingtian.wang
+ * @since 2020-11-19
+ */
+public interface UserAccountMapper extends BaseMapper<UserAccount> {
+
+}
